5bde5d095a0a0456a1e7dda250c1872ceda8f315
Leevi Graham <info@leevigraham.com>
2014-05-30 15:22:31 +1000

    Set local domain parameter

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -4,31 +4,25 @@ lock '3.2.1'
 set :application, 'symfony2-standard-template'
 set :repo_url, 'git@github.com:newism/symfony2-standard-template.git'
 
+# Files that need to remain the same between deploys
+# set :linked_files,          []
+set :linked_files, [fetch(:app_path) + '/config/parameters.yml']
+
 # curl -sS https://getcomposer.org/installer | php  -- --install-dir=/usr/local/bin --filename=composer
 SSHKit.config.command_map[:composer] = "/usr/local/bin/composer"
 set :composer_install_flags, '--no-dev --no-interaction --optimize-autoloader'
 
+
 namespace :deploy do
 
-  before :starting, 'composer:install_executable'
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+    task :copy_vendors do
+        on release_roles(:all) do
+            releases = capture(:ls, '-x', releases_path).split
+            if releases.count
+                info t("Copying vendors from previous release")
+                exec "vendorDir=#{current_path}/vendor; if [ -d $vendorDir ] || [ -h $vendorDir ]; then cp -a $vendorDir #{latest_release}; fi;"
+            end
+        end
     end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
 
 end
