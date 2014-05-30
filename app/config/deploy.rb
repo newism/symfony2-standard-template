9316c8cc120bf8369c9db0f3d8dcfb3deeb8f163
Leevi Graham <info@leevigraham.com>
2014-05-30 17:15:39 +1000

    Set permissions on cache / logs

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -9,9 +9,20 @@ set :repo_url, 'git@github.com:newism/symfony2-standard-template.git'
 set :linked_files, [fetch(:app_path) + '/config/parameters.yml']
 set :linked_dirs,  [fetch(:log_path), fetch(:web_path) + "/uploads", fetch(:app_path) + "/../vendor"]
 
-# curl -sS https://getcomposer.org/installer | php  -- --install-dir=/usr/local/bin --filename=composer
-SSHKit.config.command_map[:composer] = "/usr/local/bin/composer"
-set :composer_install_flags, '--no-dev --no-interaction --optimize-autoloader'
+set :composer_install_flags, ' --no-interaction --optimize-autoloader'
+
+# Dirs that need to be writable by the HTTP Server (i.e. cache, log dirs)
+set :file_permissions_paths, [fetch(:log_path), fetch(:cache_path)]
+set :file_permissions_users, ["www-data"]
+
+# Name used by the Web Server (i.e. www-data for Apache)
+set :webserver_user,        "www-data"
+
+# Method used to set permissions (:chmod, :acl, or :chown)
+set :permission_method,     :acl
+
+# Execute set permissions
+set :use_set_permissions,   true
 
 namespace :deploy do
   task :set_symfony_env do
@@ -22,3 +33,6 @@ end
 Capistrano::DSL.stages.each do |stage|
   after stage, 'deploy:set_symfony_env'
 end
+
+# curl -sS https://getcomposer.org/installer | php  -- --install-dir=/usr/local/bin --filename=composer
+SSHKit.config.command_map[:composer] = "/usr/local/bin/composer"
