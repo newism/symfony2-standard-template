caa59536735f17943bd715f8c2078dafd234b718
Leevi Graham <info@leevigraham.com>
2014-05-30 16:19:37 +1000

    Share vendor directory

--- app/config/deploy.rb
+++ app/config/deploy.rb
@@ -7,22 +7,8 @@ set :repo_url, 'git@github.com:newism/symfony2-standard-template.git'
 # Files that need to remain the same between deploys
 # set :linked_files,          []
 set :linked_files, [fetch(:app_path) + '/config/parameters.yml']
+set :linked_dirs,  [fetch(:log_path), fetch(:web_path) + "/uploads", fetch(:app_path) + "/../vendor"]
 
 # curl -sS https://getcomposer.org/installer | php  -- --install-dir=/usr/local/bin --filename=composer
 SSHKit.config.command_map[:composer] = "/usr/local/bin/composer"
 set :composer_install_flags, '--no-dev --no-interaction --optimize-autoloader'
-
-
-namespace :deploy do
-
-    task :copy_vendors do
-        on release_roles(:all) do
-            releases = capture(:ls, '-x', releases_path).split
-            if releases.count
-                info t("Copying vendors from previous release")
-                exec "vendorDir=#{current_path}/vendor; if [ -d $vendorDir ] || [ -h $vendorDir ]; then cp -a $vendorDir #{latest_release}; fi;"
-            end
-        end
-    end
-
-end
